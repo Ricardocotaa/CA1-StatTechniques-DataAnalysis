418432f0382f05f9b1ec970cf407aded35e4c717
Ricardocotaa <2026116@student.cct.ie>
2026-04-09 17:30:10 +0100

    Game 2 simulation

--- final_assessment.py
+++ final_assessment.py
@@ -6,9 +6,11 @@
 # Two letters, both numbers	€50
 # One letter, both numbers	€25
 # No letter, both numbers	€10
+
 import pandas as pd
 import random
 import matplotlib.pyplot as plt
+
 #Probabilities 
 
 # 3 letters correct + both numbers
@@ -28,17 +30,21 @@ Ex_v = Plllnn*100 + Pllnn*50 + Plnn*25 + Pnn*10
 
 print(Ex_v)
 0.1177600136549841
+
 #Expected value for club
 
 Ex_club = 5 - Ex_v
 print(Ex_club)
 4.882239986345016
+
 # Interpretation:
 # The player wins on average about €0.11 per game,
 # while the club earns approximately €4.88 per game.
+
 # *EXPECTED VALUE SECOND GAME*
 
 # The player pays €5 and picks 3 letters (all lower case AND upper case English letters, repetition allowed). If they guess all correctly, they win €500,000
+
 # Probability of winning
 
 Plll= 1/52*1/52*1/52
@@ -48,14 +54,17 @@ Plll= 1/52*1/52*1/52
 Ex_v2 = Plll*500000
 print(Ex_v2)
 3.55598543468366
+
 #Expected value for club
 
 Ex_club = 5 - Ex_v2
 print(Ex_club)
 1.44401456531634
+
 # Interpretation:
 # The player wins on average about €3.55 per game,
 # while the club earns approximately €1.44 per game.
+
 # *GAME 1 SIMULATION*
 def game1():
     win_data = [
@@ -72,6 +81,7 @@ def game1():
         if cumulative_propability >= random_num:
             return wins[0]
     return 0
+
 def simulate_game1(n=100000):
     total = 0
     for i in range (n):
@@ -88,7 +98,46 @@ print(gamer)
 print(club)
 0.11805
 4.88195
+
 # Interpretation:
 # The player wins about €0.11 on average per game,
 # but since they pay €5 to play, this results in a loss of about €4.88.
 # The club therefore earns approximately €4.88 per game.
+
+# *GAME 2 SIMULATION*
+
+def game2():
+    win_data = [
+        [500000, 1/52*1/52*1/52]
+    ]
+
+    random_num = random.random()
+    cumulative_probability = 0
+    
+    for wins in win_data:
+        cumulative_probability += wins[1]
+        if cumulative_probability >= random_num:
+            return wins[0]
+
+    return 0
+def simulate_game2(n=500000):
+    total = 0
+    for i in range(n):
+        total += game2()
+
+    EV_gamer = total / n
+    EV_club = 5 - EV_gamer
+
+    return EV_gamer, EV_club
+
+gamer, club = simulate_game2()
+
+print(gamer)
+print(club)
+2.0
+3.0
+
+# Interpretation:
+# The results vary because the probability of winning is very low,
+# while the prize is extremely large, making the simulation unstable.
+# Running the simulation multiple times to observe variability
