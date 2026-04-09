21aad9020b9e618ffc422e9e8998918163df700c
Ricardocotaa <2026116@student.cct.ie>
2026-04-09 15:45:59 +0100

    Expected value game 1

---
+++ final_assessment.py
@@ -0,0 +1,8 @@
+# *EXPECTED VALUE FIRST GAME*
+
+# The player pays €5 and picks 3 letters (all lower case English letters) and 2 digits (numbers 0-9 inclusive). They win as follows:
+# What is correct	Prize
+# 3 letters, both numbers	€100
+# Two letters, both numbers	€50
+# One letter, both numbers	€25
+# No letter, both numbers	€10
